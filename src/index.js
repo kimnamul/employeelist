14a27ab14a92bae7ab2e601f594f86dfce825747
yjkim01 <yjkim01@gitplecorp.com>
2022-09-19 13:20:58 +0900

    검색 구현 완료

--- src/index.js
+++ src/index.js
@@ -1,50 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-function Modal() {
-
-}
+function Modal() {}
 
 async function fetchEmployees() {
-    try {
-        const res = await fetch("https://randomuser.me/api/?results=12");
-        const { results } = await res.json();
-        console.log(results);
-        return results;
-    }
-    catch (error) {
-        console.error("fetch failure", error);
-    }
+  try {
+    const res = await fetch("https://randomuser.me/api/?results=12");
+    const { results } = await res.json();
+    return results;
+  } catch (error) {
+    console.error("fetch failure", error);
+  }
 }
 
 function Employee({ employee }) {
-    return (
-        <div className='employee-block'>
-            <img src={employee.picture.large}
-                alt="img"
-            />
-            <span className='hidden-username'>{employee.login.username}</span>
-            <h3 className='name'>{employee.name.first}</h3>
-            <ul>
-                <li>{employee.email}</li>
-                <li>{employee.location.city}</li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="employee-block">
+      <img src={employee.picture.large} alt="img" />
+      <span className="hidden-username">{employee.login.username}</span>
+      <h3 className="name">{employee.name.first}</h3>
+      <div>{employee.email}</div>
+      <div>{employee.location.city}</div>
+    </div>
+  );
 }
 
-function EmployeeList({employees}) {
-    return (
-        <div className='employee-home'>
-            {employees.map(employee => (
-                <Employee
-                    employee={employee}
-                    key={employee.login.uuid}
-                />
-            ))}
-        </div>
-    );
+function EmployeeList({ employees }) {
+  return (
+    <div className="employee-home">
+      {employees.map((employee) => (
+        <Employee employee={employee} key={employee.login.uuid} />
+      ))}
+    </div>
+  );
 }
 
 // function Search() {
@@ -80,74 +69,93 @@ function EmployeeList({employees}) {
 
 //{employees}: employee 배열
 function Search(props) {
-
-    const onSearch = (e) => {
-        e.preventDefault();
-        const value = e.target.value.toLowerCase();
-        if (!value.length) return;
-        console.log(value, props.employees);
-        const searchresult = props.employees.filter(
-            employee => employee.login.username.toLowerCase().includes(value)
-            || employee.name.first.toLowerCase().includes(value)
-        )
-        props.setEmployeeState({employee: searchresult});
-        // const searchresult = employees;
-        // searchresult.forEach((employee) => {
-        //     const name = employee.name.first.toLowerCase();
-        //     const username = employee.login.username.toLowerCase();
-        //     console.log(username, name);
-        //     if ((value && name.includes(value)) || username.includes(value)) {
-        //     } else {
-        //     }
-        // })
-        // setEmployeeState({employee: searchresult});
-    }
-
-    return (
-        <input
-            type="search"
-            placeholder="search employee directory"
-            id="search"
-            onChange={onSearch}
-        />
+  const onSearch = (e) => {
+    e.preventDefault();
+    const value = e.target.value.toLowerCase();
+    // if (!value.length) return;
+    console.log(value, props.employees);
+    const searchresult = props.employees.filter(
+      (employee) =>
+        employee.login.username.toLowerCase().includes(value) ||
+        employee.name.first.toLowerCase().includes(value)
     );
-}
+    props.setEmployeeState({ employee: searchresult });
+    // const searchresult = employees;
+    // searchresult.forEach((employee) => {
+    //     const name = employee.name.first.toLowerCase();
+    //     const username = employee.login.username.toLowerCase();
+    //     console.log(username, name);
+    //     if ((value && name.includes(value)) || username.includes(value)) {
+    //     } else {
+    //     }
+    // })
+    // setEmployeeState({employee: searchresult});
+  };
 
+  return (
+    <input
+      type="search"
+      placeholder="search employee directory"
+      id="search"
+      onChange={onSearch}
+    />
+  );
+}
 
 function Page() {
-    const [employeeState, setEmployeeState] = useState({
-        employee: []
+  const [fetchedState, setFetchedState] = useState({
+    employee: [],
+  });
+
+  const [employeeState, setEmployeeState] = useState({
+    employee: [],
+  });
+
+  useEffect(() => {
+    fetchEmployees().then((result) => {
+      console.log("result:", result);
+      setFetchedState({ employee: result });
+      setEmployeeState({ employee: result });
     });
+  }, []);
 
-    useEffect(()=>{
-        fetchEmployees().then(
-            (result)=>{
-                console.log('result:',result)
-                setEmployeeState({employee: result});
-            }
-        );
-    }, []);
-    
+  // useEffect(()=>{
+  //     console.log('use effect: ', employeeState)
+  //     return ()=>{
+  //         console.log('return callback: ', employeeState)
+  //     }
+  // }, [employeeState]);
 
-    return (
-        <div className='container'>
-            <div className='title'>
-                <h1>List of Employees</h1>
-            </div>
-            <Search setEmployeeState={setEmployeeState} employees={employeeState.employee}/>
-            <div id='employees'>
-                <EmployeeList employees={employeeState.employee}/>
-            </div>
-            <div id='modal'>
-
-            </div>
-        </div>
-    );
+  return (
+    <div className="container">
+      <div className="title">
+        <h1>List of Employees</h1>
+      </div>
+      <Search
+        setEmployeeState={setEmployeeState}
+        employees={fetchedState.employee}
+      />
+      <div id="employees">
+        <EmployeeList employees={employeeState.employee} />
+      </div>
+      <div id="modal"></div>
+    </div>
+  );
 }
+
+// function Wrapper(props) {
+//     const [show, setShow] = useState(true);
+
+//     return <div>
+//         <button onClick={()=>setShow(!show)}> SHOW</button>
+//         {show && <div {...props} ></div>}
+//     </div>
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Page />);
 
+// root.render( <Wrapper><Page /></Wrapper>);
 //setstate는 비동기적으로 한번에 실행됨
 //리액트는 직접조작을 지양
 //위에서는 employee리스트 만들고 search로 조작하여 하위로 전달
